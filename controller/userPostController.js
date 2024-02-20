31cddaf79495225fac39d62af7e91af2b840879a
Arjun <arjun346002@gmail.com>
2024-02-20 15:44:47 +0530

    multer and all other stuff

--- controller/userPostController.js
+++ controller/userPostController.js
@@ -1,28 +1,79 @@
 import userPostModel from "../models/postSchema.js";
 import registerModel from "../models/registrationSchema.js";
 
-const successUserPostController = async(req,res)=>{
+const successUserPostController = async (req, res) => {
+  try {
+    const userData = await registerModel.findById({ _id: req.session.user_id });
+    res.render("userpost", { name: userData.name });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const userPostController = async (req, res) => {
+  try {
+    const postData = await new userPostModel({
+      title: req.body.title,
+      description: req.body.description,
+      image : req.file.filename
+    });
+    await postData.save();
+    res.redirect("/dashboard");
+    console.log("Post Saved");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+const totalPostController = async (req, res) => {
+  try {
+    const allPost = await userPostModel.find({});
+    res.render("totalPost", { "allPost": allPost });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const deletePostController = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const deletePost = await userPostModel.findByIdAndDelete(req.params.id);
+    if (deletePost) {
+      console.log("Post Deleted");
+      res.redirect("/totalPost");
+    } else {
+      console.log("Post not deleted");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const editPostController = async (req, res) => {
+  try {
+    const result = await userPostModel.findById(req.params.id);
+    res.render("editPost", { result: result });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const successEditPostController = async(req,res)=>{
     try {
-        const userData = await registerModel.findById({_id:req.session.user_id});
-        res.render("userpost",{"name":userData.name});
+        const updateData = await userPostModel.findByIdAndUpdate(req.params.id,req.body);
+        if(updateData){
+            console.log("Post Updated Successfully");
+            res.redirect("/totalPost");
+        }
     } catch (error) {
         console.log(error.message);
     }
 }
 
-const userPostController = async(req,res)=>{
-    try {
-        const postData = await new userPostModel({
-            title : req.body.title,
-            description : req.body.description
-        })
-        await postData.save();
-        res.redirect("/dashboard");
-        console.log("Post Saved");
-        
-    } catch (error) {
-        console.log(error.message);
-    }
-}
-
-export {userPostController , successUserPostController};
+export {
+  userPostController,
+  successUserPostController,
+  totalPostController,
+  deletePostController,
+  editPostController,
+  successEditPostController,
+};
--- routes/web.js
+++ routes/web.js
@@ -1,25 +1,62 @@
 import express from "express";
 const router = express.Router();
-import {homeController,dashboardController} from "../controller/homeController.js";
+import multer from "multer";
+import path from "path";
+import {
+  homeController,
+  dashboardController,
+} from "../controller/homeController.js";
 import aboutController from "../controller/aboutController.js";
 import contactController from "../controller/contactController.js";
-import {loginController , userLoginController , logoutController} from "../controller/loginController.js";
-import {registerController , userRegisterController} from "../controller/registerController.js";
-import { isLogin, isLogout, registerMid } from "../middleware/userMiddleware.js";
-import { userPostController , successUserPostController } from "../controller/userPostController.js";
+import {
+  loginController,
+  userLoginController,
+  logoutController,
+} from "../controller/loginController.js";
+import {
+  registerController,
+  userRegisterController,
+} from "../controller/registerController.js";
+import {
+  isLogin,
+  isLogout,
+  registerMid,
+} from "../middleware/userMiddleware.js";
+import {
+  userPostController,
+  successUserPostController,
+  totalPostController,
+  deletePostController,
+  editPostController,
+  successEditPostController,
+} from "../controller/userPostController.js";
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(process.cwd(),"/public/images/blogimages") )
+    },
+    filename: function (req, file, cb) {
+      const img_name = Date.now() + '-' + file.originalname;
+      cb(null, img_name);
+    }
+  })
+  
+const upload = multer({ storage: storage })
 
-router.get("/",homeController);
-router.get("/about",isLogin,aboutController);
-router.get("/contact",isLogin,contactController);
-router.get("/login",isLogout,loginController);
-router.get("/register",registerMid,registerController);
+router.get("/", homeController);
+router.get("/about", isLogin, aboutController);
+router.get("/contact", isLogin, contactController);
+router.get("/login", isLogout, loginController);
+router.get("/register", registerMid, registerController);
 router.post("/register", userRegisterController);
 router.post("/login", userLoginController);
-router.get("/dashboard",isLogin,dashboardController);
-router.get("/logout",isLogin,logoutController);
-router.get("/userpost",isLogin,successUserPostController);
-router.post("/userpost",userPostController);
-
+router.get("/dashboard", isLogin, dashboardController);
+router.get("/logout", isLogin, logoutController);
+router.get("/userpost", isLogin, successUserPostController);
+router.post("/userpost", upload.single("image") ,userPostController);
+router.get("/totalPost", isLogin, totalPostController);
+router.get("/deletePost/:id", isLogin, deletePostController);
+router.get("/editPost/:id", isLogin, editPostController);
+router.post("/editPost/:id", isLogin, successEditPostController);
 
 export default router;
